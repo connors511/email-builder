d16d7ab7e8c5cfb6e57b14c331b00430b6f07bf1
Matthias Larsen <mlarsen@gammamedia.org>
2016-06-19 15:31:39 +0200

    Idk what this is..

--- client/README.md
+++ client/README.md
@@ -1,15 +1,2 @@
-# Homepage
+# Angular frontend
 *We use [EditorConfig](http://editorconfig.org/)!*
-
-*NOTE* Is the [generator-gulp-angular](https://github.com/Swiip/generator-gulp-angular) structure better than the
-one defined by the README in the parent directory?
-
-This folder contains the Angular application for the frontpage.
-
-This app should only define modules and views specific to the homeapge.
-
-_Any logic shared between this and another sub-site should be defined in [shared](https://github.com/connors511/kollegieplan/tree/master/revamp/shared)_
-
-# Usage
-Run `grunt serve` to start the server.
-It'll be accisble on port `3000` with the BrowserSync UI on port `3001`.
--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -4,7 +4,7 @@
   angular
     .module('client')
     .filter('nl2br', function(){
-        return function(msg,is_xhtml) { 
+        return function(msg,is_xhtml) {
             var is_xhtml = is_xhtml || true;
             var breakTag = (is_xhtml) ? '<br />' : '<br>';
             var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
@@ -29,7 +29,7 @@
       //socket.emit('identify', vm.name);
       vm.current = t;
     };
-    
+
     vm.create = function create() {
       vm.selectTemplate({name:'', body:''});
     }
@@ -48,49 +48,50 @@
         vm.preview = '';
         return;
       }
-      
+
       var obj = { foobar: 'bizbuzz', cheer: true };
-      
+
       var parseParent = function parseParent(name, child) {
         var tmp = _.clone(obj);
         if (child) {
           tmp.content = child;
         }
-        
+
         // Find the template
         var t = _.find(vm.templates, {name: name});
         // Parse it
-        var parsed = Liquid.Template.parse(t.body)
-                  .render(tmp);
-                  
+        // var parsed = Liquid.Template.parse(t.body)
+        //           .render(tmp);
+        var parsed = t.body.replace(/\[\[\s*content\s*\]\]/g, child);
+
         if (t.parent) {
           return parseParent(t.parent, parsed);
         }
         return parsed;
       }
-      
-      
-      
+
+
+
       try {
         var parsed = Liquid.Template.parse(_new)
-        .render(obj);
-        
+                            .render(obj);
+
         console.log("Rendered", parsed)
-        
+
         if (vm.current.parent) {
           console.log("Parsing parent")
           parsed = parseParent(vm.current.parent, parsed);
         }
-        
+
         console.log("Result", parsed)
-        
+
         vm.preview = parsed;
-      
+
       } catch (e) {
         console.log("ERR", e)
         vm.preview = e.toString();
       }
-      
+
     }, true)
 
     vm.awesomeThings = [];
